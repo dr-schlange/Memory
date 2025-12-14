1c48fa7c5f8c9bf31fc7eee61418a38a2c9be149
Nallely MIDI <drcoatl@proton.me>
2025-12-14 00:17:42 -0600

    Enhanced device class _Zab in session 0x7FFEE801B770
    SESSION_ID=0x7FFEE801B770
    original_file=/home/drschlange/memory/devices/mydevice.py

---
+++ devices/_zab.py
@@ -0,0 +1,59 @@
+from nallely import VirtualDevice, VirtualParameter, on
+from nallely import *
+
+class _Zab(VirtualDevice):
+    """
+    MyDevice
+
+    inputs:
+    # * %inname [%range] %options: %doc
+    * io0_cv [0, 127] round <increase, decrease>: first io
+    * io1_cv [0, 127] round <increase, decrease>: io
+    * in2_cv [0, 127] round <increase, decrease>: Parameter description
+    * incr_cv [0, 127] round <increase>: Parameter description
+    * decr_cv [0, 127] round <decrease>: Parameter description
+
+    outputs:
+    # * %outname [%range]: %doc
+
+    type: <ondemand | continuous>
+    category: <category>
+    # meta: disable default output
+    """
+    io0_cv = VirtualParameter(name='io0', range=(0.0, 127.0), conversion_policy='round')
+    io1_cv = VirtualParameter(name='io1', range=(0.0, 127.0), conversion_policy='round')
+    in2_cv = VirtualParameter(name='in2', range=(0.0, 127.0), conversion_policy='round')
+    incr_cv = VirtualParameter(name='incr', range=(0.0, 127.0), conversion_policy='round')
+    decr_cv = VirtualParameter(name='decr', range=(0.0, 127.0), conversion_policy='round')
+
+    @on(decr_cv, edge='decrease')
+    def on_decr_decrease(self, value, ctx):
+        ...
+
+    @on(incr_cv, edge='increase')
+    def on_incr_increase(self, value, ctx):
+        ...
+
+    @on(in2_cv, edge='increase')
+    def on_in2_increase(self, value, ctx):
+        ...
+
+    @on(in2_cv, edge='decrease')
+    def on_in2_decrease(self, value, ctx):
+        ...
+
+    @on(io1_cv, edge='increase')
+    def on_io1_increase(self, value, ctx):
+        ...
+
+    @on(io1_cv, edge='decrease')
+    def on_io1_decrease(self, value, ctx):
+        ...
+
+    @on(io0_cv, edge='increase')
+    def on_io0_increase(self, value, ctx):
+        ...
+
+    @on(io0_cv, edge='decrease')
+    def on_io0_decrease(self, value, ctx):
+        ...

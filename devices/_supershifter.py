a01e4e7cdc887c59907a9fea66012267f281d609
Nallely MIDI <drcoatl@proton.me>
2025-12-20 15:36:54 -0600

    Enhanced device class _SuperShifter in session 0x7FFEDB7EBA10
    SESSION_ID=0x7FFEDB7EBA10
    original_file=/home/drschlange/memory/devices/_supershifter.py

--- devices/_supershifter.py
+++ devices/_supershifter.py
@@ -38,5 +38,6 @@ class _SuperShifter(VirtualDevice):
         yield (0, [getattr(self, f'io{int(self.idx)}_cv')])
         if self.idx >= 3:
             self.idx = 0
-        self.idx += 1
+        else:
+            self.idx += 1
         yield (value, [getattr(self, f'io{int(self.idx)}_cv')])

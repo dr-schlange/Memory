328dc41a7947b75da1b5a898713eef588b11ef99
Nallely MIDI <drcoatl@proton.me>
2026-01-05 14:53:24 -0600

    Enhanced device class _ExpA in session 0x7FFF92087CB0
    SESSION_ID=0x7FFF92087CB0
    original_file=/home/drschlange/memory/devices/mydevice.py

---
+++ devices/_expa.py
@@ -0,0 +1,24 @@
+from mydevice import *
+from nallely import VirtualDevice, VirtualParameter, on
+
+
+class _ExpA(VirtualDevice):
+    """
+    MyDevice
+
+    inputs:
+    # * %inname [%range] %options: %doc
+    * input_cv [0, 1270, 127] <any>: input
+
+    outputs:
+    # * %outname [%range]: %doc
+
+    type: <ondemand | continuous>
+    category: <category>
+    # meta: disable default output
+    """
+
+    input_cv = VirtualParameter(name="input", range=(0.0, 1270.0))
+
+    @on(input_cv, edge="any")
+    def on_input_any(self, value, ctx): ...

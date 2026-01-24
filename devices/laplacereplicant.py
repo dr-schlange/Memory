42e2318a49dbac463d78a1bbae9a5e7df7d9c871
Nallely MIDI <drcoatl@proton.me>
2026-01-23 19:57:32 -0600

    Enhanced device class LaplaceReplicant in session 0x3A37C58C310
    SESSION_ID=0x3A37C58C310
    original_file=/home/drschlange/memory/devices/laplacereplicant.py

--- devices/laplacereplicant.py
+++ devices/laplacereplicant.py
@@ -72,7 +72,7 @@ class LaplaceReplicant(VirtualDevice):
         if self.child is None and out_now >= self.threshold:
             self.child = self.__class__()
             self.child.start()
-            self.a1_cv = self.child.output_cv.scale(0.5, -0.5)
+            self.a1_cv = self.child.output_cv.scale(1, -1)
         elif self.child is not None and out_now < self.threshold:
             self.child.stop()
             self.child = None

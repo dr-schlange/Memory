006c6386c1f4e55caf7db42ae910e2c6472d27f0
Nallely MIDI <drcoatl@proton.me>
2026-02-19 15:47:31 -0600

    Enhanced device class VCA in session 0x27B8A58D390
    SESSION_ID=0x27B8A58D390
    original_file=/home/drschlange/nallely-midi/nallely/eg.py

---
+++ devices/vca.py
@@ -0,0 +1,40 @@
+from nallely.eg import *
+from nallely import VirtualDevice, VirtualParameter, on
+
+class VCA(VirtualDevice):
+    """Voltage Controled Amplifier
+
+    Simple VCA implementation with gain
+
+    inputs:
+    * input_cv [0, 127] <any>: Input signal
+    * amplitude_cv [0.0, 1.0] init=0.0 <any>: Signal amplitude (0.0 -> 0%, 1.0 -> 100%)
+    * gain_cv [1.0, 2.0] init=1.0: Signal gain (default is 1.0)
+
+    outputs:
+    * output_cv [0, 127]: The amplified signal
+
+    type: ondemand
+    category: amplitude-modulation
+    """
+    input_cv = VirtualParameter(name='input', range=(0.0, 127.0))
+    amplitude_cv = VirtualParameter(name='amplitude', range=(0.0, 1.0), default=0.0)
+    gain_cv = VirtualParameter(name='gain', range=(1.0, 2.0), default=1.0)
+
+    @property
+    def min_range(self):
+        return 0.0
+
+    @property
+    def max_range(self):
+        return 127.0
+
+    @on(input_cv, edge='any')
+    def sending_modulated_input(self, value, ctx):
+        return value * e ^ self.amplitude * self.gain
+
+    @on(amplitude_cv, edge='any')
+    def change_amplitude(self, value, ctx):
+        if self.amplitude > 0:
+            return self.input * e ^ self.amplitude * self.gain
+        return 0
